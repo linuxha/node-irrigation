62c2aa2af9e03769e29d5bb4bc169ab5f0012c6e
Neil Cherry <ncherry@linuxha.com>
2016-11-24 17:15:54 -0500

    Added bin/git_status.sh and tests/tests.js
    I've forgotten why I added git_status.sh (must have been something I was learning about with git) but the tests.js was so I could one day run tests with mocha to test the readiness of this program. At this time I haven't got a clue how to perform and level of tests on this.
    
    [ticket: X]

---
+++ tests/tests.js
@@ -0,0 +1,8 @@
+var assert = require('assert');	// mocha
+
+describe('irrigation tests', function () {
+    it('should pass', function(done) {
+	assert.strictEqual(0,  0);
+    });
+});
+console.log('Node-irrigation tests done');
